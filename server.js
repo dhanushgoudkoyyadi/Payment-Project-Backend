6f820e25daa309588164d4fb1f49133252e8765b
K.DHANUSH GOUD <koyyadidhanushgoud@gmail.com>
2025-02-24 14:44:40 +0530

    backend

--- server.js
+++ server.js
@@ -9,5 +9,7 @@ app.use(bodyParser.json());
 mongoose.connect("mongodb+srv://dhanush:L8Xm0Ye8kO97lVop@cluster0.lecdq.mongodb.net/backend");
 var Register = require("../routes/payment-register");
 app.use("/signup", Register)
+var Payment = require("../routes/payment-details")
+app.use("/payment", Payment)
 let PORT = 5555;
 app.listen(PORT, () => { console.log("the server is running on port number 5555") })
