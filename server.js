0e08737d7964cfed2305b591e58d7d5369024988
akshaya <dakshaya0303@gmail.com>
2025-02-28 21:08:26 +0530

    route added

--- server.js
+++ server.js
@@ -161,6 +161,19 @@ app.post("/add-payment", async (req, res) => {
         res.status(500).json({ message: "Server error. Please try again." });
     }
 });
+app.get("/users/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 
 
 const PORT = 5567;
